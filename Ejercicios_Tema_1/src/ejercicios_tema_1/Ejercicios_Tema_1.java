dda7067229968a3139d688133d967e34baf00567
André Salazar <71437593+AndreSC1218@users.noreply.github.com>
2023-02-09 10:40:41 +0100

    ADD: Ejercicios_Tema_1
    ADD: Ejercicios_Tema_1

---
+++ Ejercicios_Tema_1/src/ejercicios_tema_1/Ejercicios_Tema_1.java
@@ -0,0 +1,19 @@
+package ejercicios_tema_1;
+
+public class Ejercicios_Tema_1 {
+
+    public static void main(String[] args) {
+        int edad = 30;
+        long salario = 80000L;
+        double peso = 1.75;
+        boolean esSoltero = true;
+        String nombre = "Jose Wayne";
+
+        System.out.println("Edad: " + edad);
+        System.out.println("Salario: " + salario);
+        System.out.println("Peso: " + peso);
+        System.out.println("Soltero: " + esSoltero);
+        System.out.println("Nombre: " + nombre);
+    }
+
+}
