f465e845a813db6e71af420d621907e58eec4ec9
André Salazar <71437593+AndreSC1218@users.noreply.github.com>
2023-02-11 12:31:52 +0100

    ADD: ejercicio2
    ADD: ejercicio2

---
+++ Ejercicios_Temas_7_8_y_9/src/ejercicios_temas_7_8_y_9/Ejercicio2.java
@@ -0,0 +1,16 @@
+package ejercicios_temas_7_8_y_9;
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        
+        //array unidimensional de Strings
+        String[] array = {"valor1", "valor2", "valor3", "valor4"};
+        
+        //recórremos y mostramos sus valores
+        System.out.println("Valores del array:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
